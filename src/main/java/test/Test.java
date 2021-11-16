5ff732fe70cf5e53b7c6e9f9b0d02ad499f2a579
doddi <mdodgson@sonatype.com>
2021-11-16 07:09:11 +0000

    added more test examples

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -8,8 +8,10 @@ public class Test {
 
 	public void run() {
 		if (this != null) {
+			int i = 3 + (4);
 			System.out.println("Not equal to null");
 			return;
+			int x = 0;
 		}
 		System.out.println("Equal to null");
 	}
